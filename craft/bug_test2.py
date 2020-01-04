c5d9cc8d1bc2eb5c6e07437a5fa9e2d622baeeb6
tonybear10118 <aa301875@gmail.com>
2020-01-05 03:55:28 +0800

    Add files via upload

---
+++ craft/bug_test2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import pymysql
+from craft_api import settings
+
+# test connection to mysql database
+
+connection = pymysql.connect(host=settings.MYSQL_DATABASE_HOST,
+                                 user=settings.MYSQL_DATABASE_USER,
+                                 password=settings.MYSQL_DATABASE_PASSWORD,
+                                 db=settings.MYSQL_DATABASE_DB,
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+try: 
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM `user`"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+
+finally:
+        connection.close()
+
